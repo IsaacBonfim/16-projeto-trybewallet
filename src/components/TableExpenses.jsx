132597dafbbdca87e12e6035df2423cf68161475
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-04-04 15:05:04 -0300

    Enviando o requisito 8

--- src/components/TableExpenses.jsx
+++ src/components/TableExpenses.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class TableExpenses extends React.Component {
   render() {
-    // const { expenses, dispatch } = this.props;
+    const { expenses } = this.props;
 
     return (
       <table>
@@ -21,26 +21,48 @@ class TableExpenses extends React.Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses.map((expense) => {
+            const { description, tag, method, value,
+              currency, exchangeRates, id } = expense;
+
+            return (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name.split('/')[0]}</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>
+                  {
+                    (Number(value) * Number(exchangeRates[currency].ask)).toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   expenses: [...state.wallet.expenses],
-// });
+const mapStateToProps = (state) => ({
+  expenses: [...state.wallet.expenses],
+});
 
-// TableExpenses.propTypes = {
-//   dispatch: PropTypes.func.isRequired,
-//   expenses: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       description: PropTypes.string,
-//       tag: PropTypes.string,
-//       method: PropTypes.string,
-//       value: PropTypes.string,
-//       currency: PropTypes.string,
-//     }),
-//   ).isRequired,
-// };
+TableExpenses.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string,
+      tag: PropTypes.string,
+      method: PropTypes.string,
+      value: PropTypes.string,
+      currency: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
-export default connect()(TableExpenses);
+export default connect(mapStateToProps, null)(TableExpenses);
