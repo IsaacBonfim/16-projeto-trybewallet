dc3c3e6583fe177d8ffd44b439ac850fac72da4f
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-21 20:49:10 -0300

    feat: Adicionada importação do arquivo App.css no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import './styles/App.css';
 
 function App() {
   return (
